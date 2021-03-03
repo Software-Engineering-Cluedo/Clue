248dcf4fc4aed415d74487497a0207ec659417cc
E <e@pastel.codes>
2021-03-03 15:54:36 +0000

    Changed Classes

--- src/characters.py
+++
@@ -1,9 +0,0 @@
-from cards import Cards
-
-
-class Characters(Cards):
-    def __init__(self):
-        super().__init__()
-        print('todo')
-
-    # Create some predefined characters as functions
--- src/rooms.py
+++
@@ -1,9 +0,0 @@
-from cards import Cards
-
-
-class Rooms(Cards):
-    def __init__(self):
-        super().__init__()
-        print('todo')
-
-    # Create some predefined rooms as functions
