e80f1904d8282f5bd6c65f087799604bf71ca2b1
S <au74@sussex.ac.uk>
2021-04-01 16:34:41 +0100

    added init method

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,18 +1,25 @@
 import unittest
 from src.board import Board
 
+
+
 class MyTestCase(unittest.TestCase):
+    data = []
+    
+    def __init__(self):
+        with open(self.config_dir + '/clue.json', encoding='UTF-8') as file:
+            self.data = json.loads(file.read())
+
     def test_parse_map_data(self):
         board = Board()
         result, data = board.setup_board()
         self.assertEqual(result, True)
+    
+    #def test_place_weapons_in_rooms(self):
+     #   board = Board()
+      #  result = board.place_weapons_in_rooms(w,r,s,t)
 
-    def test_yesman(self):
-        board = Board()
-        
-
-
-
+    
 
 
 if __name__ == '__main__':
