3b321720f8adbf34e4ceca0faeb55a651a0fa283
Sarah k <sarahkubati@hotmail.com>
2021-04-07 15:32:07 +0100

    Most of solution done
    once player,room, weapon complete, this can be completed

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,22 @@
+from src.room import Room
+from src.player import Player
+from src.weapon import Weapon
+
 class Solution:
     def __init__(self):
+        Room self.r = null;
+        Player self.p = null;
+        Weapon self.w = null;
         print('todo')
+
+    def generateSolution(): 
+        #randomly select one room, one player, one weapon 
+        return (r,p,w); 
+    
+    def checkSolution(wGuess, pGuess, rGuess):
+        if (wGuess==w ||pGuess==p||rGuess=r){
+            return True;
+        } else {
+            return False;
+        }
+
