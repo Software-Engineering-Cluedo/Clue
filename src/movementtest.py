3bc39ccceafbb0394bfc0e4681949e7f1b65a269
E <e@pastel.codes>
2021-04-08 16:41:46 +0100

    Removed unnecessary files for this branch

--- src/movementtest.py
+++ src/movementtest.py
@@ -1,3 +1,7 @@
 class movement_test():
     def __init__(self):
         return True
+    
+    def test_movement():
+        """ A loop to test movement showing in the terminal """
+        return True
