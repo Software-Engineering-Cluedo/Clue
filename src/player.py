c94e612c5a4b550d41518fd83b2f193001cdcac7
A <adamursell99@gmail.com>
2021-04-08 15:08:58 +0100

    functions list

--- src/player.py
+++ src/player.py
@@ -1,3 +1,11 @@
 class Player():
     def __init__(self, name, player_id, symbol):
         x = 1
+    """
+    Functions required:
+    Roll Dice
+    Check Cards
+    Make assumption (if in room)
+    Select Square to move to
+    Exit game
+    """
