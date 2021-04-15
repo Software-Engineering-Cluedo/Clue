ab8720bd31f19b556c5f819b175890e1accc99d1
Sarah k <sarahkubati@hotmail.com>
2021-04-15 23:56:45 +0100

    Removed print statement used for personal tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,7 +4,6 @@ test_game=Game()
 
 class MyTestCase(unittest.TestCase):
     def test_setup(self):
-        print(str(test_game.boardArr)+" print")
         self.assertEqual(True, True)
 
 if __name__ == '__main__':
