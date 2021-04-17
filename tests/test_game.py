6dcf55dbc70e7d9e48c125e9cce3693ad0467561
Sarah k <sarahkubati@hotmail.com>
2021-04-17 16:00:01 +0100

    Created template test functions for game
    These do NOT work, and still need to be completed properly

--- tests/test_game.py
+++ tests/test_game.py
@@ -6,6 +6,15 @@ class MyTestCase(unittest.TestCase):
     def test_setup(self):
         self.assertEqual(True, True)
 
+    def test_setup(self):
+        self.assertEqual(True,True)
+
+    def test_generate_img_tiles(self):
+        self.assertEqual(True,True)
+
+    def test_generate_accusation_window(self)
+        self.assertEqual(True,self.generate_accusation_window())
+
 if __name__ == '__main__':
     unittest.main()
 
