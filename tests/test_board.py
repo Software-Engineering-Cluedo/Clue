8978880d7488af495fe365adbce620fba80a5d26
A <adamursell99@gmail.com>
2021-04-26 15:59:55 +0100

    continuing tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
     def get_json_data(self):
         data = []
         config_dir = str(Path.home()) + "/Clue"
-        with open(config_dir + '/clue.json', encoding='UTF-8') as file:
+        with open(config_dir + 'tests\resources\json\lineDeleted.json', encoding='UTF-8') as file:
             data = json.loads(file.read())
         
         return data
