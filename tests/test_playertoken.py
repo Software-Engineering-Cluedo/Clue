953f883753494dd48cc4fcb48e9b1e48797ca756
A <adamursell99@gmail.com>
2021-04-29 11:27:35 +0100

    broken

--- tests/test_playertoken.py
+++ tests/test_playertoken.py
@@ -1,9 +1,11 @@
 import unittest
-
+from src.playertoken import PlayerToken
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_move_by_direction(self):
+        card = Card()
+        playerToken = PlayerToken(10, 10, card, board)
+        playerToken.move_by_direction(1, 1)
 
 
 if __name__ == '__main__':
