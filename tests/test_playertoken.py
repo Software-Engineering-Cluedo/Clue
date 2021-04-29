743cab53c83512617dde3ba33357b72460c14096
E <e@pastel.codes>
2021-04-29 23:36:05 +0100

    pls fix

--- tests/test_playertoken.py
+++ tests/test_playertoken.py
@@ -2,14 +2,16 @@ import unittest
 from src.playertoken import PlayerToken
 from src.card import Card
 from src.board import Board
+from src.player import Player
 
 class MyTestCase(unittest.TestCase):
 
     # Unfinished test
     def test_move_by_direction(self):
         board = Board()
-        card = Card()
-        playerToken = PlayerToken(10, 10, card, board)
+        card = Card('', 0, '')
+        player = Player('', '', '')
+        playerToken = PlayerToken(10, 10, card, board, player)
         playerToken.move_by_direction(1, 1)
 
 
