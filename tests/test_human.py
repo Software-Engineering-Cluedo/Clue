b309453d16b8d806aa3b314d63d41d1223d3eeb8
E <e@pastel.codes>
2021-04-29 23:31:09 +0100

    temp fix

--- tests/test_human.py
+++ tests/test_human.py
@@ -12,18 +12,20 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
 
-def test_suggest(self):
-        data = self.get_json_data()
-        board = Board()
-        player_token_all
+# Broken test
+#
+# def test_suggest(self):
+#         data = self.get_json_data()
+#         board = Board()
+#         player_token_all
         
-        suggest(player_token_all, player_card_dict, current_room, weapon_token_all, left_player, board)
+#         suggest(player_token_all, player_card_dict, current_room, weapon_token_all, left_player, board)
         
-        result =
+#         result =
 
-        if suggest.correct_cards != :
+#         if suggest.correct_cards != :
             
-        self.assertEqual(result, True)
+#         self.assertEqual(result, True)
 
 if __name__ == '__main__':
     unittest.main()
