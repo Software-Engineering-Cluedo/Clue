7cd0b29d23effc64c66d6d71f423a3a82ce71178
Sarah k <sarahkubati@hotmail.com>
2021-04-30 11:38:42 +0100

    Commented on player, playercard, room, solution

--- src/solution.py
+++ src/solution.py
@@ -22,11 +22,22 @@ class Solution:
 
     
     def get_solution(self):
-        """ Used for testing, gets the solution attributes """
+        """ Used for testing, gets the solution attributes 
+            Returns: 
+                tuple: room, player and weapon. 
+        """
 
         return self.room, self.player_card, self.weapon
 
 
     def check_solution(self, r_guess, p_guess, w_guess):
-        """ Checks if the given solution is correct """
+        """ Checks if the given solution is correct 
+            Args: 
+                r_guess: the guessed room
+                p_guess: the guessed player 
+                w_guess: the guessed weapon
+            Returns: 
+                boolean: true if each guess is correct, false otherwise. 
+        
+        """
         return (w_guess == list(self.weapon.values())[0] and p_guess == list(self.player_card.values())[0] and r_guess == list(self.room.values())[0])
