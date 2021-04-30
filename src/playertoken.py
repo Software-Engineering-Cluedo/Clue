2ed8d532eb54a4e7ce137e3ba4270c0ca30ac9cc
E <e@pastel.codes>
2021-04-30 11:50:35 +0100

    Added player token comments

--- src/playertoken.py
+++ src/playertoken.py
@@ -18,11 +18,22 @@ class PlayerToken(Token):
 
 
     def move(self, x, y):
+        """Moves the player to the coordanates given
+
+        Args:
+            x: x pos
+            y: y pos
+        """
         self.x = x
         self.y = y
     
     
     def move_to_room(self, room):
+        """The room to move the player to
+
+        Args:
+            room: the room to move the player to
+        """
         self.accessed_through_other_door = True
         symbol, room = list(room.items())[0]
         self.current_room = symbol
@@ -31,6 +42,16 @@ class PlayerToken(Token):
 
 
     def move_by_direction(self, off_x, off_y):
+        """Moves the player by particular offest, and if a door is met, enter it
+
+        Args:
+            off_x: the offset to move the x by
+            off_y: the offset to move the y by
+        
+        Returns:
+            bool: if has moved successfully move, return True
+            bool: if the user has just entered a room via a door
+        """
         cur_x, cur_y = self.get_position()
         temp_x, temp_y = [cur_x + off_x, cur_y + off_y]
         if temp_y >= 0 and temp_x >= 0 and temp_y < self.board.data['map']['dimensions']['y'] and temp_x < self.board.data['map']['dimensions']['x'] and self.board.player_map[temp_y][temp_x] == '' and (self.board.door_map[temp_y][temp_x] == self.board.default_symbols['door'] or self.board.tile_map[temp_y][temp_x] == self.board.default_symbols['tile']):
@@ -56,6 +77,7 @@ class PlayerToken(Token):
 
 
     def enter_secret_door(self):
+        """Makes the user enter a secret door"""
         found = False
         self.has_entered = True
         current_room_secret_door = list(self.board.secret_door_rooms[self.current_room].keys())[0]
@@ -83,6 +105,7 @@ class PlayerToken(Token):
     
 
     def exit_door(self):
+        """Exits the door depending if the room was entered via a secret door or normal door"""
         if self.accessed_through_other_door:
             self.exit_secret_door()
         else:
@@ -90,6 +113,7 @@ class PlayerToken(Token):
 
 
     def exit_normal_door(self):
+        """Exists a normal door"""
         x, y = self.door_entered
         self.move(x, y)
 
@@ -99,6 +123,7 @@ class PlayerToken(Token):
     
 
     def exit_secret_door(self):
+        """ Exits a room through a randomly assigned door from the room you are in """
         all_tiles_next_to_doors = self.board.get_door_offset(self.current_room, self.board.tile_map, self.board.door_rooms, self.board.default_symbols['tile'])
         temp_x, temp_y = random.choice(all_tiles_next_to_doors)
         self.move(temp_x, temp_y)
@@ -109,10 +134,12 @@ class PlayerToken(Token):
     
 
     def reset_has_entered(self):
+        """Sets has entered to false"""
         self.has_entered = False
     
 
     def get_turn_options(self):
+        """Gets the list of options for that turn"""
         if self.door_entered == None:
             return 0 # Move normally
         elif self.current_room in self.board.secret_door_rooms and self.has_entered:
